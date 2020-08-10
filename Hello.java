8b086d4d0b2b975baf0567ca7c3e51262eefbf7b
Kim Sang Min <yhoo112@naver.com>
2020-08-10 11:09:08 +0900

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+		System.out.println("Nice to meet you! 222");
+		int myAge = 27;
+		System.out.println("My Age is " + myAge);
+	}
+}
