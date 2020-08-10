dd98175d5addf023f770fe6f5938caaa4cc1e80f
Kim Sang Min <yhoo112@naver.com>
2020-08-10 12:15:14 +0900

    second commit

---
+++ VariableExample.java
@@ -0,0 +1,8 @@
+public class VariableExample {
+	public static void main(String[] args) {
+		int value1 = 10;
+		int value2 = 20;
+		int result = value1 + value2;
+		system.out.println("10 + 20 = " + result);		
+	}
+}
